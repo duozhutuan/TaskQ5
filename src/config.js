eac173ded8a6276a2812539ed71d6a4a4114957c
asmcos <asmcos@blx.com>
2024-11-29 10:26:02 +0800

    modify channel id

--- src/config.js
+++ src/config.js
@@ -1,22 +1,33 @@
 export let channel_info = {
-  id: '27bc0c85d8b52a4c016b8c22790447da94f819609cb597babdf3603a82e5fbbe',
+  content: '{"name":"TaskQ5","about":"TaskQ5 is a task distribution platform where you can post tasks if you need help.","picture":"https://raw.githubusercontent.com/duozhutuan/taskq5/refs/heads/main/docs/logo.png","relays":["wss://relay1.nostrchat.io","wss://relay2.nostrchat.io","wss://purplerelay.com/","wss://relay.damus.io","wss://strfry.iris.to","wss://nos.lol","wss://nostr.slothy.win","wss://xmr.usenostr.org","wss://at.nostrworks.com","wss://btc.klendazu.com","wss://relay.nostrified.org","wss://news.utxo.one","wss://bitcoinmaximalists.online/","wss://nostr.tbai.me:592/"]}',
+  created_at: 1732847026,
+  id: '33dc840f93f36b4bbdd84244200ab1c8b2f299e2033d40731eefc8ca3af78281',
   kind: 40,
   pubkey: 'bccf33d867d2fb0b02905297efd8dc9edabae6576214b9ef0c636ab94b705625',
-  created_at: 1732151769,
-  content: '{"name": "taskq5 requests", "about": "TaskQ5 is a task distribution platform where you can post tasks if you need help.", "picture": "https://duozhutuan.com/logo.svg","relays": [  "wss://relay1.nostrchat.io","wss://relay2.nostrchat.io","wss://relay.damus.io","wss://relay.snort.social","wss://nos.lol"]}',
+  sig: '8fc5cf19ec3c57df94131cfd69442adfb372853c45866ada57d5346c656d3fb1e7fb0d60e7be4564630c63be4bf395f0569201ce995f649f82f77b6eff3e016b',
   tags: [],
-  sig: '7a85a205c4669f689437aa4abb9588c932f153cfb53e9b43ab4a388f2e9c5d7a140d3b244b1dbf3d19c8f8b000961e9597d6b19c2ae05d7df5eb2febc835a188'
 }
 
-export let relayServer =  [  'wss://relay1.nostrchat.io',
+export let relayServer =  [  
+  'wss://relay1.nostrchat.io',
   'wss://relay2.nostrchat.io',
+  'wss://purplerelay.com/',
   'wss://relay.damus.io',
   'wss://strfry.iris.to',
   'wss://nos.lol',
+  'wss://nostr.slothy.win',
+  'wss://xmr.usenostr.org',
+  'wss://at.nostrworks.com',  
+  'wss://btc.klendazu.com',
+  'wss://relay.nostrified.org',
+  'wss://news.utxo.one'
+  'wss://bitcoinmaximalists.online/',
+  'wss://nostr.tbai.me:592/',
+
 ];
 
-let hub = "wss://bridge.duozhutuan.com/";
-//let hub = "";
+//let hub = "wss://bridge.duozhutuan.com/";
+let hub = "";
 export let relays = relayServer.map(relay => hub + relay );
 
 export const rejectSelfTasks = true;
